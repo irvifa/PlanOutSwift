1d9570bea3da7ee1a4b74c4a03e9144906bca3f1
Irvi Firqotul Aini <irvi.fa@gmail.com>
2020-04-29 22:40:45 +0700

    Fix build.

---
+++ File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  PlanOutSwift
+//
+//  Created by irvi on 29/04/20.
+//
+
+import Foundation
--- Source/PlanOutSwift.h
+++ Source/PlanOutSwift.h
@@ -1,11 +1,13 @@
 //
 //  PlanOutSwift.h
 //  PlanOutSwift
-
-
+//
 #import <UIKit/UIKit.h>
 
+//! Project version number for PlanOutSwift.
 FOUNDATION_EXPORT double PlanOutSwiftVersionNumber;
 
+//! Project version string for PlanOutSwift.
 FOUNDATION_EXPORT const unsigned char PlanOutSwiftVersionString[];
 
+// In this header, you should import all the public headers of your framework using statements like #import <PlanOutSwift/PublicHeader.h>
